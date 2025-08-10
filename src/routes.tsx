de562a099e21b6a1577e4a3c07aa886b7a64ce3f
elias estrabao <eestrabao46@gmail.com>
2025-08-09 20:31:02 -0400

    routing system

---
+++ src/routes.tsx
@@ -0,0 +1,25 @@
+
+import { lazy, Suspense, type JSX } from 'react';
+import type { RouteObject } from 'react-router-dom';
+
+import MainLayout from '@/components/Layout/MainLayout';
+
+const Home = lazy(() => import('@/pages/Home'))
+const Login = lazy(() => import('@/pages/Login'));
+const Register = lazy(() => import('@/pages/Register'));
+
+const withSuspense = (el: JSX.Element) => (
+  <Suspense fallback={<div>Loading…</div>}>{el}</Suspense>
+);
+
+export const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <MainLayout/>,
+    children: [
+      { index: true, element: withSuspense(<Home />) },
+      { path: '/login', element: withSuspense(<Login />) },
+      { path: '/register', element: withSuspense(<Register />) },
+    ]
+  }
+];
