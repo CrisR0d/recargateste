6ca96be28f3d7d369dc7c3c537426d93fd87c1ef
elias estrabao <eestrabao46@gmail.com>
2025-08-30 19:12:23 -0400

    feat: added route products

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,6 +7,7 @@ import MainLayout from '@/components/Layout/MainLayout';
 const Home = lazy(() => import('@/pages/Home'))
 const Login = lazy(() => import('@/pages/Login'));
 const Register = lazy(() => import('@/pages/Register'));
+const ProductOptions = lazy(() => import('@/pages/Product'));
 
 const withSuspense = (el: JSX.Element) => (
   <Suspense fallback={<div>Loading…</div>}>{el}</Suspense>
@@ -20,6 +21,8 @@ export const routes: RouteObject[] = [
       { index: true, element: withSuspense(<Home />) },
       { path: '/login', element: withSuspense(<Login />) },
       { path: '/register', element: withSuspense(<Register />) },
+      { path: '/product', element: withSuspense(<Register />) },
+      { path: '/product/:productName', element: withSuspense(<ProductOptions />) },
     ]
   }
 ];
